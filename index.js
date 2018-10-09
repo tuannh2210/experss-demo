f626996559f5211d539efb6bb75499f4a5a9b46b
Tuannh2210 <tuannh2210@gmail.com>
2018-10-09 15:35:44 +0700

    Update search

--- index.js
+++ index.js
@@ -5,15 +5,33 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+	{id:1, name: 'Tuan'},
+	{id:2, name: 'Nguyễn'},
+]
+
 app.get('/',function(req,res){
 	res.render('index',{
-		users:[
-			{id:1, name: 'Tuan'},
-			{id:1, name: 'Tuan'},
-			{id: 2 , name:'Agiih'}
-		]
+		users:  users
 	});
 });
+
+app.get('/users',function(req,res){
+	res.render('users',{
+		users: users
+	});
+});
+
+app.get('/users/search',function(req,res ) {
+	var q = req.query.q;
+	var matchedUser = users.filter(function(user){
+		return user.name.indexOf(q) !== -1;
+	});
+	res.render('user/index',{
+		users:matchedUser
+	});
+});
+
 
 app.get('/tuan',function(req,res){
 	res.send('<h1> TUANA</h1>')
